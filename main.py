65775237ef6230ff754949c2fd56f06c35f1bdc7
Dumiii <dumitras.diaconu.dumi@gmail.com>
2021-02-18 21:01:24 +0000

    small changes

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import requests
+import json
+from pprint import pprint
 
-r = requests.get("https://api.warframe.market/v1/items")
+r = requests.get("https://api.warframe.market/v1/items/rubico_prime_blueprint")
 
-print(r.json())
+# with open("item_info.txt", "w") as write_file:
+#    json.dump(r.json(), write_file)
+
+with open("item_info.txt", "r") as handle:
+   parsed = json.load(handle)
+
+pprint(parsed)
